2e9b4d190910d5ad4a53eca568495ade4cd1e751
bhagirathifunde08 <fundebhagirathi@gmail.com>
2026-08-08 14:53:23 +0530

    Dynamic task list for app.py

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask ,render_template
 app =Flask(__name__)
 
 todos = [
-    {"id": 1, "task": "Learn Flask","desc":"this is first task", "status": "pending"}]
+    {"id": 1, "task": "Learn Flask",
+     "desc":"this is sample task for todo list", "date_created":"08-08-26", "status":"pending"}
+     ]
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html",allTodos =todos)
 
 if __name__ == "__main__":
     app.run(debug=True ,port=8000)
