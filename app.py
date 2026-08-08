746ff871373e4786a3dfa3cd60cfd23060fa5e5a
= <=>
2026-08-08 17:15:01 +0530

    add files

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask ,render_template
+from flask import Flask,render_template
 
-app =Flask(__name__)
+app = Flask(__name__)
 
 todos = [
-    {"id": 1, "task": "Learn Flask",
-     "desc":"this is sample task for todo list", "date_created":"08-08-26", "status":"pending"}
-     ]
+    {"sno":1,"title":"HomeWork","desc":"Complete All Assignments","date_created":"08-08-2026","status":"pending"}
+]
+
 @app.route("/")
 def home():
-    return render_template("index.html",allTodos =todos)
+    return render_template("index.html",allTodos = todos)
+
 
 if __name__ == "__main__":
-    app.run(debug=True ,port=8000)
-
+    app.run(debug=True,port=8000)
